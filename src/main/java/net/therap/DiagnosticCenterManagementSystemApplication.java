9ae9484c93d31a1a9298ee6b68a0dc885e5494d7
Raian Rahman <raian.rahman@therapservices.net>
2022-08-01 09:32:48 +0600

    Project startup

---
+++ src/main/java/net/therap/DiagnosticCenterManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package net.therap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosticCenterManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiagnosticCenterManagementSystemApplication.class, args);
+    }
+
+}
