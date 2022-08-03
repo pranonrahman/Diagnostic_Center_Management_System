734b784f6c536a2fe4cbfb003f6be6de02924716
Raian Rahman <raian.rahman@therapservices.net>
2022-08-03 12:27:10 +0600

    Updated DataLoader.java

--- src/main/java/net/therap/bootstrap/DataLoader.java
+++ src/main/java/net/therap/bootstrap/DataLoader.java
@@ -31,7 +31,7 @@ public class DataLoader implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         createRole();
     }
 }
