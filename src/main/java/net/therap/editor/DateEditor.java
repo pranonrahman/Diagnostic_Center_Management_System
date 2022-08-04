e28c0fdcef5cc1a9469c1a5976c756ed7b2761e4
Raian Rahman <raian.rahman@therapservices.net>
2022-08-04 12:57:28 +0600

    Added DateEditor

---
+++ src/main/java/net/therap/editor/DateEditor.java
@@ -0,0 +1,30 @@
+package net.therap.editor;
+
+import org.springframework.stereotype.Component;
+
+import java.beans.PropertyEditorSupport;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import static java.util.Objects.isNull;
+
+/**
+ * @author raian.rahman
+ * @since 8/4/22
+ */
+@Component
+public class DateEditor extends PropertyEditorSupport {
+
+    @Override
+    public void setAsText(String text) {
+        if(isNull(text) || text.isBlank()) {
+            setValue(null);
+        } else {
+            try {
+                setValue(new SimpleDateFormat("yyyy-MM-dd").parse(text));
+            } catch (ParseException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+    }
+}
