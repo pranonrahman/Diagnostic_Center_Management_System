8ec72479e739c2b19cba424fcb36272ce37532d0
Raian Rahman <raian.rahman@therapservices.net>
2022-08-04 11:19:51 +0600

    Added role property editor

---
+++ src/main/java/net/therap/editor/RoleEditor.java
@@ -0,0 +1,27 @@
+package net.therap.editor;
+
+import net.therap.model.Role;
+import net.therap.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.beans.PropertyEditorSupport;
+
+/**
+ * @author raian.rahman
+ * @since 8/3/22
+ */
+@Component
+public class RoleEditor extends PropertyEditorSupport {
+
+    @Autowired
+    private RoleService roleService;
+
+    @Override
+    public void setAsText(String roleId) throws IllegalArgumentException {
+        if(!roleId.isEmpty()) {
+            Role role = roleService.findById(Long.valueOf(roleId));
+            setValue(role);
+        }
+    }
+}
