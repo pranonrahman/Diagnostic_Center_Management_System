01a85ee8a3edb162eaff5ad24831cdfa4b0a3950
Raian Rahman <raianrahman@iut-dhaka.edu>
2022-08-07 22:43:10 +0600

    Added equals and hashcode for Role

--- src/main/java/net/therap/model/Role.java
+++ src/main/java/net/therap/model/Role.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Table;
+import java.util.Objects;
 
 /**
  * @author raian.rahman
@@ -27,5 +28,19 @@ public class Role extends BaseEntity {
 
     public Role(RoleEnum name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Role role = (Role) o;
+        return name == role.name;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), name);
     }
 }
