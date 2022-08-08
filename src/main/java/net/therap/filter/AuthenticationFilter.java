d2429e4f389ddfd1f4fc4162d52244618a51194c
Raian Rahman <raianrahman@iut-dhaka.edu>
2022-08-09 00:03:58 +0600

    Refactored AuthenticationFilter to make all link as constant

--- src/main/java/net/therap/filter/AuthenticationFilter.java
+++ src/main/java/net/therap/filter/AuthenticationFilter.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 import static java.util.Objects.isNull;
+import static net.therap.constant.UriConstant.*;
 import static net.therap.model.RoleEnum.*;
 
 /**
@@ -43,11 +44,12 @@ public class AuthenticationFilter implements Filter {
 
         if ((isNull(httpServletRequest.getSession().getAttribute("user"))
                 || isNull(httpServletRequest.getSession().getAttribute("role")))
-                && !httpServletRequest.getRequestURI().contains("/login")
-                && !httpServletRequest.getRequestURI().contains("/login/role")
-                && !httpServletRequest.getRequestURI().contains("/logout")
-                && !httpServletRequest.getRequestURI().contains("/assets/")
-                && !httpServletRequest.getRequestURI().contains("/favicon.ico")
+                && !httpServletRequest.getRequestURI().contains(LOGIN)
+                && !httpServletRequest.getRequestURI().contains(HOME)
+                && !httpServletRequest.getRequestURI().contains(LOGIN_ROLE)
+                && !httpServletRequest.getRequestURI().contains(LOGOUT)
+                && !httpServletRequest.getRequestURI().contains(ASSETS)
+                && !httpServletRequest.getRequestURI().contains(FAV_ICON)
         ) {
 
             httpServletResponse.sendRedirect(LOGIN_REDIRECT_PATH);
@@ -57,7 +59,7 @@ public class AuthenticationFilter implements Filter {
 
         Role role = (Role) httpServletRequest.getSession().getAttribute("role");
 
-        if (httpServletRequest.getRequestURI().contains("/invoice/view")
+        if (httpServletRequest.getRequestURI().contains(INVOICE_VIEW)
                 && !receptionistRole.equals(role)
                 && !patientRole.equals(role)) {
 
@@ -66,8 +68,13 @@ public class AuthenticationFilter implements Filter {
             return;
         }
 
-        if ((httpServletRequest.getRequestURI().contains("/invoice")
-                || httpServletRequest.getRequestURI().contains("/invoice/list"))
+        if ((httpServletRequest.getRequestURI().contains(INVOICE_SAVE)
+                || httpServletRequest.getRequestURI().contains(INVOICE_LIST)
+                || httpServletRequest.getRequestURI().contains(INVOICE_DOCTOR)
+                || httpServletRequest.getRequestURI().contains(INVOICE_FACILITY)
+                || httpServletRequest.getRequestURI().contains(INVOICE_MEDICINE)
+                || httpServletRequest.getRequestURI().contains(INVOICE_LIST)
+        )
                 && !receptionistRole.equals(role)) {
 
             httpServletResponse.sendRedirect(INVALID_ACCESS_REDIRECT_PATH);
@@ -75,7 +82,8 @@ public class AuthenticationFilter implements Filter {
             return;
         }
 
-        if (httpServletRequest.getRequestURI().contains("/prescription/view")
+        if ((httpServletRequest.getRequestURI().contains(PRESCRIPTION_VIEW)
+                || httpServletRequest.getRequestURI().contains(PRESCRIPTION_LIST))
                 && !doctorRole.equals(role)
                 && !patientRole.equals(role)) {
 
@@ -84,14 +92,14 @@ public class AuthenticationFilter implements Filter {
             return;
         }
 
-        if (httpServletRequest.getRequestURI().contains("/prescription/save") && !doctorRole.equals(role)) {
+        if (httpServletRequest.getRequestURI().contains(PRESCRIPTION_SAVE) && !doctorRole.equals(role)) {
 
             httpServletResponse.sendRedirect(INVALID_ACCESS_REDIRECT_PATH);
 
             return;
         }
 
-        if (httpServletRequest.getRequestURI().contains("/patient/history")
+        if (httpServletRequest.getRequestURI().contains(PATIENT_HISTORY)
                 && !doctorRole.equals(role)
                 && !patientRole.equals(role)) {
 
@@ -100,18 +108,10 @@ public class AuthenticationFilter implements Filter {
             return;
         }
 
-        if (httpServletRequest.getRequestURI().contains("/doctor/patients")
-                && !doctorRole.equals(role)) {
-            System.out.println(httpServletRequest.getRequestURI());
-            httpServletResponse.sendRedirect(INVALID_ACCESS_REDIRECT_PATH);
-
-            return;
-        }
-
-        if ((httpServletRequest.getRequestURI().contains("/person/list")
-                || httpServletRequest.getRequestURI().contains("/person/save")
-                || httpServletRequest.getRequestURI().contains("/person/delete")
-                || httpServletRequest.getRequestURI().contains("/person/view"))
+        if ((httpServletRequest.getRequestURI().contains(PERSON_LIST)
+                || httpServletRequest.getRequestURI().contains(PERSON_SAVE)
+                || httpServletRequest.getRequestURI().contains(PERSON_DELETE)
+                || httpServletRequest.getRequestURI().contains(PERSON_VIEW))
                 && !adminRole.equals(role)) {
 
             httpServletResponse.sendRedirect(INVALID_ACCESS_REDIRECT_PATH);
