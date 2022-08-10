1c73f96d19b05874a56325f825db5ec8b3b531f1
Raian Rahman <raian.rahman@therapservices.net>
2022-08-10 16:25:46 +0600

    Refactored Services and Filter

--- src/main/java/net/therap/service/AuthenticationService.java
+++ src/main/java/net/therap/service/AuthenticationService.java
@@ -32,7 +32,7 @@ public class AuthenticationService {
 
     public boolean authenticateByRole(UserViewModel userViewModel, User user) {
 
-        if(isNull(userViewModel)) {
+        if (isNull(userViewModel)) {
             return false;
         }
 
