b72d059d256bfbd5239d967dcd89e0233462ae1f
Raian Rahman <raian.rahman@therapservices.net>
2022-08-11 15:26:21 +0600

    Created controller for home

---
+++ src/main/java/net/therap/controller/DashboardController.java
@@ -0,0 +1,22 @@
+package net.therap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author raian.rahman
+ * @since 8/11/22
+ */
+@Controller
+@RequestMapping({"/","/home"})
+public class DashboardController {
+
+    private static final String DASHBOARD = "dashboard";
+
+    @GetMapping
+    public String showDashboard() {
+        return DASHBOARD;
+    }
+}
