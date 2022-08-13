ed62e45bc56ed7c301175fbf4e7e1192001e4a5d
khandaker.maruf <khandaker.maruf@therapservices.net>
2022-08-13 22:31:43 +0600

    Error message read from message.properties

--- src/main/java/net/therap/validator/DoctorVisitCmdValidator.java
+++ src/main/java/net/therap/validator/DoctorVisitCmdValidator.java
@@ -1,6 +1,8 @@
 package net.therap.validator;
 
 import net.therap.command.DoctorVisitCmd;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
@@ -9,6 +11,10 @@ import org.springframework.validation.Validator;
  * @since 8/13/22
  */
 public class DoctorVisitCmdValidator implements Validator {
+
+    @Autowired
+    MessageSourceAccessor msa;
+
     @Override
     public boolean supports(Class<?> clazz) {
         return clazz.equals(DoctorVisitCmd.class);
@@ -16,14 +22,14 @@ public class DoctorVisitCmdValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
-        DoctorVisitCmd doctorVisit= (DoctorVisitCmd) target;
+        DoctorVisitCmd doctorVisit = (DoctorVisitCmd) target;
 
         boolean selfAppointment = doctorVisit.getDoctors()
                 .stream()
                 .anyMatch(doctor -> doctor.getId() == doctorVisit.getPatient().getId());
 
-        if(selfAppointment) {
-            errors.rejectValue("doctors", "", "Can't set appointment with thyself");
+        if (selfAppointment) {
+            errors.rejectValue("doctors", "", msa.getMessage("error.selfAppointment"));
         }
     }
 }
