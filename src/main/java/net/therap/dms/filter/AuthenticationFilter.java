71a7feec8dc70e8151bf00921195d33cdf1122a0
Raian Rahman <raian.rahman@therapservices.net>
2022-08-15 21:01:26 +0600

    Moved methods to AccessControlHelper

--- src/main/java/net/therap/dms/filter/AuthenticationFilter.java
+++ src/main/java/net/therap/dms/filter/AuthenticationFilter.java
@@ -1,9 +1,8 @@
 package net.therap.dms.filter;
 
 import net.therap.dms.constant.URL;
-import net.therap.dms.entity.Role;
 import net.therap.dms.entity.User;
-import net.therap.dms.service.RoleService;
+import net.therap.dms.helper.AccessControlHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static java.util.Objects.nonNull;
-import static net.therap.dms.entity.RoleEnum.*;
 import static net.therap.dms.util.SessionUtil.getUser;
 
 /**
@@ -27,24 +24,12 @@ public class AuthenticationFilter implements Filter, URL {
     private static final String HOME_REDIRECT_PATH = "/";
     private static final String INVALID_ACCESS_REDIRECT_PATH = "/invalidPage";
 
-    private static final String USER = "user";
-
-    private Role adminRole;
-    private Role doctorRole;
-    private Role patientRole;
-    private Role receptionistRole;
-
     @Autowired
-    private RoleService roleService;
+    private AccessControlHelper accessControlHelper;
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-
-        adminRole = roleService.findByName(ADMIN);
-        doctorRole = roleService.findByName(DOCTOR);
-        patientRole = roleService.findByName(PATIENT);
-        receptionistRole = roleService.findByName(RECEPTIONIST);
 
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
@@ -53,7 +38,7 @@ public class AuthenticationFilter implements Filter, URL {
         httpServletResponse.setHeader("Pragma", "no-cache");
         httpServletResponse.setDateHeader("Expires", 0);
 
-        if(isLoggedIn(httpServletRequest)) {
+        if(accessControlHelper.isLoggedIn(httpServletRequest)) {
             User user = getUser(httpServletRequest);
 
             if(httpServletRequest.getRequestURI().contains(LOGIN)) {
@@ -61,10 +46,10 @@ public class AuthenticationFilter implements Filter, URL {
                 return;
             }
 
-            if (!hasInvoiceAccess(httpServletRequest, user)
-                    || !hasPatientAccess(httpServletRequest, user)
-                    || !hasUserAccess(httpServletRequest, user)
-                    || !hasPrescriptionAccess(httpServletRequest, user)) {
+            if (!accessControlHelper.hasInvoiceAccess(httpServletRequest, user)
+                    || !accessControlHelper.hasPatientAccess(httpServletRequest, user)
+                    || !accessControlHelper.hasUserAccess(httpServletRequest, user)
+                    || !accessControlHelper.hasPrescriptionAccess(httpServletRequest, user)) {
 
                 httpServletResponse.sendRedirect(INVALID_ACCESS_REDIRECT_PATH);
 
@@ -72,7 +57,7 @@ public class AuthenticationFilter implements Filter, URL {
             }
         }
 
-        if(!isLoggedIn(httpServletRequest)
+        if(!accessControlHelper.isLoggedIn(httpServletRequest)
             && !httpServletRequest.getRequestURI().equals(LOGIN)
             && !httpServletRequest.getRequestURI().equals(LOGOUT)
             && !httpServletRequest.getRequestURI().contains(ASSETS)
@@ -84,52 +69,5 @@ public class AuthenticationFilter implements Filter, URL {
 
 
         chain.doFilter(request, response);
-    }
-
-    private boolean isLoggedIn(HttpServletRequest request) {
-        return nonNull(request.getSession().getAttribute(USER));
-    }
-
-    private boolean hasInvoiceAccess(HttpServletRequest request, User user) {
-        if ((request.getRequestURI().equals(INVOICE)
-                || request.getRequestURI().contains(INVOICE_LIST))
-                && !user.getRoles().contains(receptionistRole)
-                && !user.getRoles().contains(patientRole)) {
-            return false;
-        }
-
-        return (!request.getRequestURI().contains(INVOICE_DOCTOR)
-                && !request.getRequestURI().contains(INVOICE_FACILITY)
-                && !request.getRequestURI().contains(INVOICE_MEDICINE))
-                || user.getRoles().contains(receptionistRole);
-    }
-
-    private boolean hasUserAccess(HttpServletRequest request, User user) {
-        return (!request.getRequestURI().contains(USER_LIST)
-                && !request.getRequestURI().contains(USER_VIEW)
-                && !request.getRequestURI().contains(USER_DELETE))
-                || user.getRoles().contains(adminRole);
-    }
-
-    private boolean hasPatientAccess(HttpServletRequest request, User user) {
-        if (request.getRequestURI().contains(PATIENT_LIST) && !user.getRoles().contains(doctorRole)) {
-            return false;
-        }
-
-        return !request.getRequestURI().contains(PATIENT_HISTORY)
-                || user.getRoles().contains(doctorRole)
-                || user.getRoles().contains(patientRole);
-    }
-
-    private boolean hasPrescriptionAccess(HttpServletRequest request, User user) {
-        if ((request.getRequestURI().equals(PRESCRIPTION)
-                || request.getRequestURI().contains(PRESCRIPTION_LIST))
-                && !user.getRoles().contains(doctorRole)
-                && !user.getRoles().contains(patientRole)) {
-            return false;
-        }
-
-        return !request.getRequestURI().contains(PRESCRIPTION_SAVE)
-                || user.getRoles().contains(doctorRole);
     }
 }
