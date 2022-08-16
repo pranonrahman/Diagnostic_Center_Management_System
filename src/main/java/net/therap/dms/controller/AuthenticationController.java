06d41add7a22b796f327e2bb8ca01faceabacdfe
Raian Rahman <raian.rahman@therapservices.net>
2022-08-16 11:26:34 +0600

    Added group validation while logging in

--- src/main/java/net/therap/dms/controller/AuthenticationController.java
+++ src/main/java/net/therap/dms/controller/AuthenticationController.java
@@ -1,21 +1,22 @@
 package net.therap.dms.controller;
 
-import net.therap.dms.command.UserCmd;
 import net.therap.dms.editor.RoleEditor;
 import net.therap.dms.entity.Role;
+import net.therap.dms.entity.User;
 import net.therap.dms.service.UserService;
-import net.therap.dms.validator.UserCmdValidator;
+import net.therap.dms.validationGroup.UserAuthenticationGroup;
+import net.therap.dms.validator.AuthenticationValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
 import static net.therap.dms.constant.URL.LOGIN;
 import static net.therap.dms.constant.URL.LOGOUT;
@@ -29,14 +30,11 @@ import static net.therap.dms.controller.AuthenticationController.USER;
 @SessionAttributes(USER)
 public class AuthenticationController {
 
+    public static final String USER = "user";
     private static final String LOGIN_REDIRECT_PATH = "redirect:/login";
     private static final String DASHBOARD_REDIRECT_PATH = "redirect:/";
     private static final String FORM_PAGE = "/authentication/form";
-
     private static final String USER_CMD = "userCmd";
-
-    public static final String USER = "user";
-
     @Autowired
     private RoleEditor roleEditor;
 
@@ -44,12 +42,12 @@ public class AuthenticationController {
     private UserService userService;
 
     @Autowired
-    private UserCmdValidator userCmdValidator;
+    private AuthenticationValidator authenticationValidator;
 
     @InitBinder
     private void initBinder(WebDataBinder webDataBinder) {
         webDataBinder.registerCustomEditor(Role.class, roleEditor);
-        webDataBinder.addValidators(userCmdValidator);
+        webDataBinder.addValidators(authenticationValidator);
         webDataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
     }
 
@@ -62,7 +60,7 @@ public class AuthenticationController {
     }
 
     @PostMapping(LOGIN)
-    public String process(@Valid @ModelAttribute(USER_CMD) UserCmd userCmd,
+    public String process(@Validated(UserAuthenticationGroup.class) @ModelAttribute(USER_CMD) User user,
                           BindingResult result,
                           HttpServletRequest request) {
 
@@ -70,7 +68,7 @@ public class AuthenticationController {
             return FORM_PAGE;
         }
 
-        request.getSession().setAttribute(USER, userService.findByUserName(userCmd.getUserName()));
+        request.getSession().setAttribute(USER, userService.findByUserName(user.getUserName()));
 
         return DASHBOARD_REDIRECT_PATH;
     }
@@ -83,6 +81,6 @@ public class AuthenticationController {
     }
 
     private void setUpReferenceData(ModelMap model) {
-        model.put(USER_CMD, new UserCmd());
+        model.put(USER_CMD, new User());
     }
 }
